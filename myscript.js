be16b00cc6b42e2905d3da9d9adf58a15e0f368b
RedEarthRecreation <126543747+RedEarthRecreation@users.noreply.github.com>
2023-03-11 12:08:48 -0400

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -7,17 +7,17 @@ fetch(imageFolder)
     // Extract the list of image file names from the HTML response
     const parser = new DOMParser();
     const htmlDoc = parser.parseFromString(data, "text/html");
-    const imageFiles = Array.from(htmlDoc.querySelectorAll("a")).map(a => a.href);
+    const imageFiles = Array.from(htmlDoc.querySelectorAll("a"))
+      .map(a => a.href.replace(window.location.href, ""))
+      .filter(file => /\.(jpe?g|png)$/i.test(file));
 
     // Create and append image elements to the .row div
     const rowDiv = document.querySelector(".row");
     imageFiles.forEach(imageFile => {
-      if (imageFile.endsWith(".jpg") || imageFile.endsWith(".jpeg") || imageFile.endsWith(".png")) {
-        const imageElement = document.createElement("img");
-        imageElement.src = imageFolder + imageFile;
-        imageElement.alt = imageFile;
-        rowDiv.appendChild(imageElement);
-      }
+      const imageElement = document.createElement("img");
+      imageElement.src = imageFolder + imageFile;
+      imageElement.alt = imageFile;
+      rowDiv.appendChild(imageElement);
     });
   })
   .catch(error => {
