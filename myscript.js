9cac751da0ac8352d71304b0d166ef4b3734146e
RedEarthRecreation <126543747+RedEarthRecreation@users.noreply.github.com>
2023-03-11 11:33:50 -0400

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,27 +1,30 @@
 document.addEventListener("DOMContentLoaded", function() {
+  const imagesFolder = "./flora/";
+  const row = document.querySelector(".row");
 
-
-  const imagesFolder = "http://www.yourdomain.com/flora/"; // absolute path
-
-const row = document.querySelector(".row");
-
-fetch(imagesFolder)
-  .then(response => response.text())
-  .then(html => {
-    const parser = new DOMParser();
-    const doc = parser.parseFromString(html, "text/html");
-    const links = doc.querySelectorAll("a");
-    const images = [];
-    links.forEach(link => {
-      const href = link.getAttribute("href");
-      if (href.match(/\.(jpe?g|png|gif)$/)) {
-        images.push(imagesFolder + href);
-      }
+  fetch(imagesFolder)
+    .then(response => response.text())
+    .then(html => {
+      const parser = new DOMParser();
+      const doc = parser.parseFromString(html, "text/html");
+      const links = doc.querySelectorAll("a");
+      const images = [];
+      links.forEach(link => {
+        const href = link.getAttribute("href");
+        if (href.match(/\.(jpe?g|png|gif)$/)) {
+          const imagePath = imagesFolder + href;
+          images.push(imagePath);
+          console.log(`Found image: ${imagePath}`);
+        }
+      });
+      images.forEach(image => {
+        const img = document.createElement("img");
+        img.src = image;
+        row.appendChild(img);
+        console.log(`Appended image: ${image}`);
+      });
+    })
+    .catch(error => {
+      console.log(`Error fetching images: ${error}`);
     });
-    images.forEach(image => {
-      const img = document.createElement("img");
-      img.src = image;
-      row.appendChild(img);
-    });
-  });
 });
